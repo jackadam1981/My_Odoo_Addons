3a7da433d6278c400ef232ee4348d8323d7e2570
jack <jackadam@sina.com>
2022-07-03 18:12:38 +0800

    写中文吧，反正是中国身份证，外国人也不会用。 升级15.0.0.2

--- identification_strengthen/__manifest__.py
+++ identification_strengthen/__manifest__.py
@@ -3,30 +3,38 @@
     'name': "身份证号加强",
 
     'summary': """
-        身份证号自动计算性别，生日""",
+        身份证号自动计算性别，生日，年龄""",
 
     'description': """
         使用身份证号计算生日
             6-14位是8位的生日数字字符串
         使用身份证号计算性别
             16-17位，偶数是女性，基数是男性
+        使用身份证号计算年龄
+            能算生日，就能算年龄，但是不能只是在更新身份证号的时候计算，而要每天计算。
+            计算两个年龄:
+                一个是按生日算的年龄，real_age
+                一个只算年，当前年减出生年，calculate_age
+        服务器任务时间需要自己去调一下，默认是安装模块的这个时间，想调整为夜间跑任务，只要改一下下次执行时间即可。
+        
     """,
 
     'author': "自由工作室",
-    'website': "http://www.baidu.com",
+    'website': "http://www.bing.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Human Resources',
-    'version': '0.1',
+    'version': '15.0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr'],
 
     # always loaded
     'data': [
-        'views/views.xml'
+        'views/calculate_age_cron.xml',
+        'views/hr_view.xml',
 
     ],
     # only loaded in demonstration mode
--- identification_strengthen/models/models.py
+++ identification_strengthen/models/models.py
@@ -2,6 +2,7 @@
 import datetime
 
 from odoo import models, fields, api
+from dateutil.relativedelta import relativedelta
 
 
 class identification_strengthen(models.Model):
@@ -14,17 +15,50 @@ class identification_strengthen(models.Model):
     ], groups="hr.group_hr_user", compute='_gender_pc', tracking=True)
 
     birthday = fields.Date('Date of Birth', groups="hr.group_hr_user", tracking=True)
+    real_age = fields.Integer(string='真实年龄(算生日)')
+    calculate_age = fields.Integer(string='简单年龄(只算年)')
 
     @api.depends('identification_id')
     def _gender_pc(self):
         for record in self:
-            gender_check = record.identification_id[16:17]
-            if (int(gender_check) % 2) == 0:
-                result = 'female'
-            else:
-                result = 'male'
-            record.gender = result
+            try:
+                gender_check = record.identification_id[16:17]
+                print(gender_check)
+                if (int(gender_check) % 2) == 0:
+                    result = 'female'
+                else:
+                    result = 'male'
+                record.gender = result
 
-            birth_check = record.identification_id[6:14]
-            birth_day = datetime.datetime.strptime(birth_check, "%Y%m%d")
-            record.birthday = birth_day
+                birth_check = record.identification_id[6:14]
+                birth_day = datetime.datetime.strptime(birth_check, "%Y%m%d")
+                record.birthday = birth_day
+            except:
+                record.gender = 'other'
+
+    @api.model
+    def _calculate_age(self):
+        # Called by a cron
+        # 计算年龄
+        today_d = datetime.date.today()
+        all_employee = self.search([])
+        for i in all_employee:
+            try:
+                birth_check = str(i.identification_id)[6:14]
+                birth_d = datetime.datetime.strptime(birth_check, "%Y%m%d")
+                try:
+                    birth_t = i.birthday.replace(year=today_d.year)
+                except ValueError:  # raised when birth date is February 29 and the current year is not a leap year
+                    birth_t = i.birthday.replace(year=today_d.year, month=self.birthday.month + 1, day=1)
+                finally:
+                    print(today_d, birth_t)
+                    print(type(today_d), type(birth_t))
+                    if today_d > birth_t:
+                        age = today_d.year - birth_d.year
+                    else:
+                        age = today_d.year - birth_d.year - 1
+                    print(age)
+                    i.real_age = age
+                i.calculate_age = today_d.year - birth_d.year
+            except:
+                print('bed')
